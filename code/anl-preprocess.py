af5da6e08df05c8885c9bb11e5fc3160bdb49a43
gregbubnis <gbubnis@gmail.com>
2020-01-08 13:20:22 -0800

    factored out featurization

--- code/anl-preprocess.py
+++ code/anl-preprocess.py
@@ -19,175 +19,7 @@ import numpy as np
 
 import remtools as rt
 import scoreblock as sb
-
-
-def make_spectrogram_params():
-    """defaults"""
-    pEEG = dict(lowpass=20,  highpass=2,  logscale=False, normalize=True, medianfilter=9, stride=5)
-    pEMG = dict(lowpass=100, highpass=130, logscale=False, normalize=True, medianfilter=9, stride=10)
-    dd = dict(spectrogram=dict(EEG=pEEG, EMG=pEMG))
-    return dd
-
-
-def compute_spectrogram_features(edfd=None, params={}):
-    """compute spectrogram based features
-    
-    WWRW: spectrogram specific things are contained here, features used
-        elsewhere are not assumed to derive from spectrograms
-
-    TODO: rename powspec
-
-    input
-    ------
-    edfd (EDFData)
-    params
-
-    output
-    ------
-    scoreblock
-
-    """
-
-    # defaults
-    default_params = make_spectrogram_params()
-    pEEG = default_params['spectrogram']['EEG']
-    pEMG = default_params['spectrogram']['EMG']
-
-    pEEG.update(params.get('EEG', {}))
-    pEMG.update(params.get('EMG', {}))
-
-    # preprocess each spectrogram
-    EEG1 = edfd.spectrograms['EEG1'].prep(pEEG).to_df
-    EEG2 = edfd.spectrograms['EEG2'].prep(pEEG).to_df
-    EMG = edfd.spectrograms['EMG'].prep(pEMG).to_df
-
-    # build a scoreblock
-    dd = dict(EEG1=EEG1, EEG2=EEG2, EMG=EMG)
-    df = pd.concat(dd).reset_index().rename(columns={'level_0': 'channel'})
-    index_cols = ['channel','f[Hz]']
-
-    scb = sb.ScoreBlock(df=df, index_cols=index_cols)
-
-    return scb
-
-def make_rmspower_params():
-    """defaults"""
-    p1 = dict(
-        tag='EMG_mfw00',
-        channel='EMG',
-        epochLength=10,
-        medianFilterWidth=0,
-        lowcut=100,
-        highcut=130
-        )
-    p2 = dict(
-        tag='EMG_mfw90',
-        channel='EMG',
-        epochLength=10,
-        medianFilterWidth=90,
-        lowcut=100,
-        highcut=130
-        )
-    dd = dict(rmspower=[p1, p2])
-    return dd
-
-
-
-def compute_power_features(edfd=None, params=None):
-    """compute rms power features
-
-    TODO: rename rmspow
-    TODO: extract the inner function
-    TODO: for this to work as intented, every featurization needs to use the
-          same epochLength.
-
-    - params contains a list of parameter dicts
-    - each dict generates one RMSpower featurization
-    - this allows for chopping input signals into diffrent bands
-    - the output is a scoreblock of stacked feature row vectors (not really
-        scores, but its a useful data container)
-
-    input
-    ------
-    edfd (EDFData)
-    params
-
-    output
-    ------
-    scoreblock
-    """
-
-    import tsm1d
-    from scipy import signal
-
-    if params is None:
-        params = make_rmspower_params()['rmspower']
-
-    scoreblocks = []
-    for pp in params:
-
-        print('--------------')
-        print('rmspower featurization:')
-        print(pp)
-
-        tag = pp['tag']
-        channel = pp['channel']
-        epochLength = pp['epochLength']
-        mfw = pp['medianFilterWidth']
-        lowcut = pp['lowcut']
-        highcut = pp['highcut']
-
-        s = edfd.signal_traces[channel]
-
-        # bandpass
-        s = s.bandpass(lowcut=lowcut, highcut=highcut)
-
-        # window (array) size
-        wsize = int(s.f*epochLength)
-
-        # median filter width (array)
-        mfwidth = int(np.floor(mfw/epochLength/2)*2+1)
-
-        # compute rms power, epoch block averaged
-        stsq = s.sig**2
-        rmspow = np.sqrt(tsm1d.block_avg(stsq, wsize))
-        logpow = np.log(rmspow)
-
-        # standardize
-        logpow -= np.mean(logpow)
-        logpow /= np.std(logpow)
-
-        # time values (block centers)
-        tval = (np.arange(len(logpow))+0.5)*epochLength
-
-        # the result
-        logpowmf = signal.medfilt(logpow, kernel_size=[mfwidth])
-
-        # build a scoreblock (of features)
-        index = dict(
-            tag=tag,
-            medianFilterWidth=mfw,
-            epochLength=epochLength,
-            channel=channel
-        )
-
-        data_cols = ['epoch-%5.5i' % (i+1) for i in range(len(logpowmf))]
-        index_cols = [k for k in index.keys()]
-        data = [v for v in index.values()] + logpowmf.tolist()
-        df = pd.Series(index=index_cols+data_cols, data=data).to_frame().T
-        scb = sb.ScoreBlock(df=df, index_cols=index_cols)
-
-        scoreblocks.append(scb)
-
-    if len(scoreblocks) >1:
-        fblk = scoreblocks[0].stack(others=scoreblocks[1:])
-    else:
-        fblk = scoreblocks[0]
-
-    return fblk
-
-
-
+import featurize as fz
 
 #==============================================================================
 #=============================== main program =================================
@@ -230,10 +62,10 @@ for index, row in load.iterrows():
     # featurize
     if 'spectrogram' in jdic.keys():
         params = jdic['spectrogram']
-        features_scb = compute_spectrogram_features(edfd=edf, params=params)
+        features_scb = fz.compute_powspec_features(edfd=edf, params=params)
     elif 'rmspower' in jdic.keys():
         params = jdic['rmspower']
-        features_scb = compute_power_features(edfd=edf, params=params)
+        features_scb = fz.compute_rmspow_features(edfd=edf, params=params)
     else:
         raise Exception('params not recognized')
 
---
+++ code/featurize.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+
+#
+#   featurization schemes
+#
+#   powspec:
+#       - computes the power spectrum for each epoch
+#       - smooth, normalize, bandpass, stride
+#
+#   rmspow:
+#       - not using the DFT, numerically safer for short epochs
+#       - bandpass and compute block averaged rms power
+#       - power is log scaled and the result is standardized (zero mean, unit variance)
+#
+
+import pdb
+
+import pandas as pd
+import numpy as np
+
+import scoreblock as sb
+
+
+def make_powspec_params():
+    """defaults"""
+    pEEG = dict(lowpass=20,  highpass=2,  logscale=False, normalize=True, medianfilter=9, stride=5)
+    pEMG = dict(lowpass=100, highpass=130, logscale=False, normalize=True, medianfilter=9, stride=10)
+    dd = dict(spectrogram=dict(EEG=pEEG, EMG=pEMG))
+    return dd
+
+
+def compute_powspec_features(edfd=None, params={}):
+    """compute spectrogram based features
+    
+    WWRW: spectrogram specific things are contained here, features used
+        elsewhere are not assumed to derive from spectrograms
+
+    TODO: rename powspec
+
+    input
+    ------
+    edfd (EDFData)
+    params
+
+    output
+    ------
+    scoreblock
+
+    """
+
+    # defaults
+    default_params = make_spectrogram_params()
+    pEEG = default_params['spectrogram']['EEG']
+    pEMG = default_params['spectrogram']['EMG']
+
+    pEEG.update(params.get('EEG', {}))
+    pEMG.update(params.get('EMG', {}))
+
+    # preprocess each spectrogram
+    EEG1 = edfd.spectrograms['EEG1'].prep(pEEG).to_df
+    EEG2 = edfd.spectrograms['EEG2'].prep(pEEG).to_df
+    EMG = edfd.spectrograms['EMG'].prep(pEMG).to_df
+
+    # build a scoreblock
+    dd = dict(EEG1=EEG1, EEG2=EEG2, EMG=EMG)
+    df = pd.concat(dd).reset_index().rename(columns={'level_0': 'channel'})
+    index_cols = ['channel','f[Hz]']
+
+    scb = sb.ScoreBlock(df=df, index_cols=index_cols)
+
+    return scb
+
+def make_rmspow_params():
+    """defaults"""
+    p1 = dict(
+        tag='EMG_mfw00',
+        channel='EMG',
+        epochLength=10,
+        medianFilterWidth=0,
+        lowcut=100,
+        highcut=130
+        )
+    p2 = dict(
+        tag='EMG_mfw90',
+        channel='EMG',
+        epochLength=10,
+        medianFilterWidth=90,
+        lowcut=100,
+        highcut=130
+        )
+    dd = dict(rmspower=[p1, p2])
+    return dd
+
+
+def compute_rmspow_features(edfd=None, params=None):
+    """compute rms power features
+
+    TODO: rename rmspow
+    TODO: extract the inner function
+    TODO: for this to work as intented, every featurization needs to use the
+          same epochLength.
+
+    - params contains a list of parameter dicts
+    - each dict generates one RMSpower featurization
+    - this allows for chopping input signals into diffrent bands
+    - the output is a scoreblock of stacked feature row vectors (not really
+        scores, but its a useful data container)
+
+    input
+    ------
+    edfd (EDFData)
+    params
+
+    output
+    ------
+    scoreblock
+    """
+
+    import tsm1d
+    from scipy import signal
+
+    if params is None:
+        params = make_rmspower_params()['rmspower']
+
+    scoreblocks = []
+    for pp in params:
+
+        print('--------------')
+        print('rmspower featurization:')
+        print(pp)
+
+        tag = pp['tag']
+        channel = pp['channel']
+        epochLength = pp['epochLength']
+        mfw = pp['medianFilterWidth']
+        lowcut = pp['lowcut']
+        highcut = pp['highcut']
+
+        s = edfd.signal_traces[channel]
+
+        # bandpass
+        s = s.bandpass(lowcut=lowcut, highcut=highcut)
+
+        # window (array) size
+        wsize = int(s.f*epochLength)
+
+        # median filter width (array)
+        mfwidth = int(np.floor(mfw/epochLength/2)*2+1)
+
+        # compute rms power, epoch block averaged
+        stsq = s.sig**2
+        rmspow = np.sqrt(tsm1d.block_avg(stsq, wsize))
+        logpow = np.log(rmspow)
+
+        # standardize
+        logpow -= np.mean(logpow)
+        logpow /= np.std(logpow)
+
+        # time values (block centers)
+        tval = (np.arange(len(logpow))+0.5)*epochLength
+
+        # the result
+        logpowmf = signal.medfilt(logpow, kernel_size=[mfwidth])
+
+        # build a scoreblock (of features)
+        index = dict(
+            tag=tag,
+            medianFilterWidth=mfw,
+            epochLength=epochLength,
+            channel=channel
+        )
+
+        data_cols = ['epoch-%5.5i' % (i+1) for i in range(len(logpowmf))]
+        index_cols = [k for k in index.keys()]
+        data = [v for v in index.values()] + logpowmf.tolist()
+        df = pd.Series(index=index_cols+data_cols, data=data).to_frame().T
+        scb = sb.ScoreBlock(df=df, index_cols=index_cols)
+
+        scoreblocks.append(scb)
+
+    if len(scoreblocks) >1:
+        fblk = scoreblocks[0].stack(others=scoreblocks[1:])
+    else:
+        fblk = scoreblocks[0]
+
+    return fblk
+
+
+
+if __name__ == "__main__":
+
+    pass
