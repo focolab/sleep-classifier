32fea3c68e8a75b95e75f7059c746e141d96e738
gregbubnis <gbubnis@gmail.com>
2020-01-07 16:43:16 -0800

    confusion matrix plotting relocated

--- code/plottools.py
+++ code/plottools.py
@@ -27,6 +27,67 @@ sns.set_style('ticks')
 
 
 
+def plot_confusion_matrix(ax=None,
+                          cm=None,
+                          classes=None,
+                          normalize=False,
+                          cbar=True,
+                          title=None,
+                          cmap=plt.cm.Blues,
+                          imkwa={},
+                          colorkwa={}):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    #cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #print("plotting normalized confusion matrix")
+    else:
+        pass
+        #print('plotting confusion matrix, without normalization')
+
+    #print(cm)
+
+    if ax is None:
+        fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap, **imkwa)
+    if cbar:
+        ax.figure.colorbar(im, ax=ax, **colorkwa)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    #fig.tight_layout()
+    return ax
+
 
 def plot_features_template(df_feat_index=None, unique_scores=None, 
                            xpad=2, boxkwa=None,
